3b256435d98dbf6ad7c65687ef6c97486c42b50c
Varunram <vrg2009@ymail.com>
2019-04-01 17:39:44 +0530

    change formdata to raw json

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func GetRequest(url string) ([]byte, error) {
+func Get(url string) ([]byte, error) {
 	var dummy []byte
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
@@ -45,7 +45,7 @@ func responseHandler(w http.ResponseWriter, status int) {
 	default:
 		response.Status = "404 Page Not Found"
 	}
-	MarshalSend(w, response)
+	Send(w, response)
 }
 
 func WriteToHandler(w http.ResponseWriter, jsonString []byte) {
@@ -56,7 +56,7 @@ func WriteToHandler(w http.ResponseWriter, jsonString []byte) {
 	w.Write(jsonString)
 }
 
-func MarshalSend(w http.ResponseWriter, x interface{}) {
+func Send(w http.ResponseWriter, x interface{}) {
 	xJson, err := json.Marshal(x)
 	if err != nil {
 		log.Println("did not marshal json", err)
@@ -68,8 +68,8 @@ func MarshalSend(w http.ResponseWriter, x interface{}) {
 
 // GetAndSendJsonBalance is a handler that makes a get request and returns json data
 func GetBalanceAddress(w http.ResponseWriter, r *http.Request, addr string) (float64, float64) {
-	body := "http://34.73.144.32:443/address/" + addr
-	data, err := GetRequest(body)
+	body := "http://localhost:443/address/" + addr
+	data, err := Get(body)
 	if err != nil {
 		log.Println("did not get response", err)
 		return -1, -1
@@ -83,6 +83,101 @@ func GetBalanceAddress(w http.ResponseWriter, r *http.Request, addr string) (flo
 	}
 
 	return x.ChainStats.Funded_txo_sum, x.MempoolStats.Funded_txo_sum
+}
+
+type Tx struct {
+	Txid     string  `json:"txid"`
+	Version  float64 `json:"version"`
+	Locktime float64 `json:"locktime"`
+	Vin      []struct {
+		Txid    string  `json:"txid"`
+		Vout    float64 `json:"vout"`
+		PrevOut struct {
+			Scriptpubkey         string  `json:"scriptpubkey"`
+			Scriptpubkey_asm     string  `json:"scriptpubkey_asm"`
+			Scriptpubkey_address string  `json:"scriptpubkey_address"`
+			Scriptpubkey_type    string  `json:"scriptpubkey_type"`
+			Value                float64 `json:"value"`
+		} `json:"prevout"`
+		Scriptsig     string  `json:"scriptsig"`
+		Scriptsig_asm string  `json:"scriptsig_asm"`
+		Witness       string  `json:"witness"`
+		Is_coinbase   bool    `json:"is_coinbase"`
+		Sequence      float64 `json:"sequence"`
+	} `json:"vin"`
+	Vout []struct {
+		Scriptpubkey         string  `json:"scriptpubkey"`
+		Scriptpubkey_asm     string  `json:"scriptpubkey_asm"`
+		Scriptpubkey_address string  `json:"scriptpubkey_address"`
+		Scriptpubkey_type    string  `json:"scriptpubkey_type"`
+		Value                float64 `json:"value"`
+	}
+	Size   float64 `json:"size"`
+	weight float64 `json:"weight"`
+	Fee    float64 `json:"fee"`
+	Status struct {
+		Confirmed    bool    `json:"confirmed"`
+		Block_height float64 `json:"block_height"`
+		Block_hash   string  `json:"block_hash"`
+		Block_time   float64 `json:"block_time"`
+	}
+}
+
+func GetTxsAddress(w http.ResponseWriter, r *http.Request, addr string) ([]Tx, error) {
+	var x []Tx
+	body := "http://localhost:443/address/" + addr + "/txs"
+	log.Println(body)
+	data, err := Get(body)
+	if err != nil {
+		log.Println("did not get response", err)
+		return x, err
+	}
+	// now data is in byte, we need the other structure now
+	err = json.Unmarshal(data, &x)
+	if err != nil {
+		log.Println("did not unmarshal json", err)
+		return x, err
+	}
+
+	return x, nil
+}
+
+type Utxo struct {
+	Txid string `json:"txid"`
+	Vout []struct {
+		Scriptpubkey         string  `json:"scriptpubkey"`
+		Scriptpubkey_asm     string  `json:"scriptpubkey_asm"`
+		Scriptpubkey_address string  `json:"scriptpubkey_address"`
+		Scriptpubkey_type    string  `json:"scriptpubkey_type"`
+		Value                float64 `json:"value"`
+	}`json:"vout"`
+	Status struct {
+		Confirmed bool `json:"confirmed"`
+		Block_height float64 `json:"block_height"`
+		Block_hash string `json:"block_hash"`
+		Block_time float64 `json:"block_time"`
+	}
+	Value float64 `json:"value"`
+}
+
+// curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Origin: localhost" -H "Cache-Control: no-cache" -d 'addresses=17rdSE552fTwvRqLxdKJtfkncB1om8XtJT%2C17rdSE552fTwvRqLxdKJtfkncB1om8XtJT%2C17rdSE552fTwvRqLxdKJtfkncB1om8XtJT' "http://localhost:3001/multigetutxos"
+func GetUtxosAddress(w http.ResponseWriter, r *http.Request, addr string) ([]Utxo, error) {
+	var x []Utxo
+	body := "http://localhost:443/address/" + addr + "/txs"
+	log.Println(body)
+	data, err := Get(body)
+	if err != nil {
+		log.Println("did not get response", err)
+		return x, err
+	}
+	// now data is in byte, we need the other structure now
+	err = json.Unmarshal(data, &x)
+	if err != nil {
+		log.Println("did not unmarshal json", err)
+		return x, err
+	}
+
+	return x, nil
 }
 
 type GetBalanceFormat struct {
@@ -120,20 +215,26 @@ type MultigetBalance struct {
 	UnconfirmedBalance float64
 }
 
+type RequestFormat struct {
+	Addresses []string `json:"addresses"`
+}
+// example request:
+// curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Origin: localhost" -H "Cache-Control: no-cache" -d 'addresses=17rdSE552fTwvRqLxdKJtfkncB1om8XtJT%2C17rdSE552fTwvRqLxdKJtfkncB1om8XtJT%2C17rdSE552fTwvRqLxdKJtfkncB1om8XtJT' "http://localhost:3001/multigetbalance"
 func multigetBalance() {
 	// make a curl request out to lcoalhost and get the ping response
 	http.HandleFunc("/multigetbalance", func(w http.ResponseWriter, r *http.Request) {
 		// validate if the person requesting this is a vlaid user on the platform
-		checkGetRequest(w, r) // check origin of request as well if needed
-		var arr []string
-		err := r.ParseForm()
+		checkPostRequest(w, r) // check origin of request as well if needed
+		data, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			log.Println("did not parse form", err)
-			return prepProject, err
+			log.Fatal(err)
 		}
-		addresses := r.FormValue("addresses")
-		// this is a comma separated list. Parse that.
-		arr := string.Split(addresses, ",")
+		var rf RequestFormat
+		err = json.Unmarshal(data, &rf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		arr := rf.Addresses
 		balance := float64(0)
 		uBalance := float64(0)
 		for _, elem := range arr {
@@ -145,15 +246,80 @@ func multigetBalance() {
 		var x MultigetBalance
 		x.Balance = balance
 		x.UnconfirmedBalance = uBalance
-		MarshalSend(w, x)
+		Send(w, x)
+	})
+}
+
+// curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -H "Origin: localhost" -H "Cache-Control: no-cache" -d 'addresses=17rdSE552fTwvRqLxdKJtfkncB1om8XtJT%2C17rdSE552fTwvRqLxdKJtfkncB1om8XtJT%2C17rdSE552fTwvRqLxdKJtfkncB1om8XtJT' "http://localhost:3001/multigettxs"
+func multigetTxs() {
+	// make a curl request out to lcoalhost and get the ping response
+	http.HandleFunc("/multigettxs", func(w http.ResponseWriter, r *http.Request) {
+		// validate if the person requesting this is a vlaid user on the platform
+		checkPostRequest(w, r) // check origin of request as well if needed
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var rf RequestFormat
+		err = json.Unmarshal(data, &rf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		arr := rf.Addresses
+		var result [][]Tx
+		for _, elem := range arr {
+			// send the request out
+			tempTxs, err := GetTxsAddress(w, r, elem)
+			if err != nil {
+				log.Println(err)
+				responseHandler(w, http.StatusInternalServerError)
+				return
+			}
+			result = append(result, tempTxs)
+		}
+		Send(w, result)
+	})
+}
+
+func multigetUtxos() {
+	// make a curl request out to lcoalhost and get the ping response
+	http.HandleFunc("/multigetutxos", func(w http.ResponseWriter, r *http.Request) {
+		// validate if the person requesting this is a vlaid user on the platform
+		checkPostRequest(w, r) // check origin of request as well if needed
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var rf RequestFormat
+		err = json.Unmarshal(data, &rf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		arr := rf.Addresses
+		var result [][]Utxo
+		for _, elem := range arr {
+			// send the request out
+			tempTxs, err := GetUtxosAddress(w, r, elem)
+			if err != nil {
+				log.Println(err)
+				responseHandler(w, http.StatusInternalServerError)
+				return
+			}
+			result = append(result, tempTxs)
+		}
+		Send(w, result)
 	})
 }
 
 func startHandlers() {
 	multigetBalance()
+	multigetTxs()
+	multigetUtxos()
 }
 
 func main() {
 	startHandlers()
-	log.Fatal(http.ListenAndServe("3001", nil)) // 3001 os hardcoded as of now
+	// if you're running esplora, use socat tcp-listen:3003,reuseaddr,fork tcp:localhost:3002 to tunnel port since
+	// it does not seem possible to open the port directly
+	log.Fatal(http.ListenAndServe("localhost:3001", nil)) // 3001 os hardcoded as of now
 }
